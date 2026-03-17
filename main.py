caf36c5ffc673e464a8a45e6831702f646654650
dushiyu <dushiyu123@icloud.com>
2026-03-17 15:52:10 +0800

    增加test/login接口

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from unit.log import Logger
 from vo.sapVo import sapVo
 
 app = Flask(__name__)
+# ========== 关键配置：强制UTF-8编码 ==========
+app.config['JSON_AS_ASCII'] = False  # 禁用ASCII编码，返回原生中文
+app.config['JSONIFY_MIMETYPE'] = 'application/json; charset=utf-8'  # 响应头指定UTF-8
 
 @app.route('/api/zmdms-esb-sap/sap/open/call', methods=['POST'])
 def sap_call():
@@ -30,6 +33,18 @@ def sap_call():
         res_data = SapData.get_data(year=year, month=month, prctr=prctr)
     Logger.info(res_data)
     return Response.success(data=res_data)
+
+
+@app.route('/test/login',methods=['GET'])
+def test():
+    if request.args.get('username') == None or request.args.get('password') == None:
+        return Response.error({"msg":"用户名或密码不能为空"})
+    elif request.args.get('username') == 'admin' and request.args.get('password') != 'admin':
+        return Response.error({"msg":"用户名或密码错误"})
+    elif request.args.get('username') == 'admin' and request.args.get('password') == 'admin':
+        return Response.success({"msg":"登陆成功"})
+    else:
+        return Response.error({"msg":"登录异常"})
 
 if __name__ == '__main__':
     app.run(debug=False)
